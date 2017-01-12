f379a730cef655bcce36d7b3330356e3f89a847b
Unknown <brandonreyes230495@gmail.com>
2017-01-12 05:36:22 -0800

    VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,15 @@
 <template>
-    <div id="app" class="ui middle aligned center aligned grid">
-	  <div class="column">
-	    <h2 class="ui teal image header">
-	      <img src="./icons/logo-small.png" class="image">
-	      <div class="content">
-	        Log-in to your account
-	      </div>
-	    </h2>
-	    <form class="ui large form">
-	      <div class="ui stacked segment">
-	        <div class="field">
-	          <div class="ui left icon input">
-	            <i class="user icon"></i>
-	            <input type="text" name="email" placeholder="E-mail address">
-	          </div>
-	        </div>
-	        <div class="field">
-	          <div class="ui left icon input">
-	            <i class="lock icon"></i>
-	            <input type="password" name="password" placeholder="Password">
-	          </div>
-	        </div>
-	        <div class="ui fluid large teal submit button">Login</div>
-	      </div>
-	      <div class="ui error message"></div>
-	    </form>
-
-	    <div class="ui message">
-	      New to us? <a href="#">Sign Up</a>
-	    </div>
-	  </div>
-	</div>
+	<Login></Login>
 </template>
 
 <script>
-import './semantic/dist/semantic.css'
+	import './semantic/dist/semantic.css'
+	import Login from './components/Login'
 
-export default {
-  name: 'app'
-}
+	export default {
+		name: 'app',
+		components: {
+			Login
+		}
+	}
 </script>
-
-<style>
-	body > .grid {
-		height: 100%;
-	}
-
-	.column {
-		max-width: 450px;
-	}
-</style>
